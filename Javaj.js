3c1e0273af364427ed6e2686f9938edf4e50b1da
Andreiko Dmytro <144222985+Dimaa411@users.noreply.github.com>
2023-11-27 20:06:45 +0200

    Update Javaj.js

--- Javaj.js
+++ Javaj.js
@@ -1,207 +1,221 @@
-const prompt = require("prompt-sync")()
-const colors = { reset: '\x1b[0m', green: '\x1b[32m' }
-const deliveryPrise = 5050/100
-let statusDelivery = null
-let person = null
-let myOrder = null
+const prompt = require('prompt-sync')();
 
+const person = {
+  name: '',
+  email: '',
+  phone: '',
+};
+ 
+const delivery = {
+  delivery_price: 400,
+  from: "вул.Коновальця",
+  to: '',
+};
+ 
+const order = {
+  ...person,
+  ...delivery,
+  dishes: [],
+  finalPrice: '',
+  status: 'В обробці', 
+};
 
-const menu = [
+chooseRole();
+
+function chooseRole() {
+  let enterAs;
+
+  do {
+    enterAs = prompt('Оберіть 1 -- вхід для користувача, 2 -- вхід для адміна, 3 -- вийти з програми: ');
+
+    if (enterAs === '1') {
+      Usermain();
+    } 
+    if (enterAs === '2') {
+      adminMain();
+    } 
+    if (enterAs === '3') {
+      console.log('Гарного дня');
+    } else {
+      console.log('Невірний вибір. Будь ласка, оберіть 1, 2 або 3.');
+    }
+  } while (enterAs !== '3');
+}
+
+function Usermain() {
+  const menu = [
     {
-        category: "Pizza",
-        items: [
-            {
-                title: "Salami",
-                price: 12000,
-                weight: "500 g",
-                ingredients: ['Dough', 'Salami', 'Cheese', 'Souse'],
-                adress: "Pizza Avenue 46a"
-            },
-            {
-                title: "Hawaii",
-                price: 14500,
-                weight: "500 g",
-                ingredients: ['Dough', 'Pineapple', 'Cheese', 'Olives', 'Souse'],
-                adress: "Pizza Avenue 46a"
-            },
-        ]
+      ID: '1',
+      title: 'california',
+      price: 7000,
+      category: 'sushi',
+      ingredients: ['rice', 'souce', 'sea fish'],
     },
     {
-        category: "Sushi",
-        items: [
-            {
-                title: "California",
-                price: 70000,
-                weight: "1 kg",
-                ingredients: ['Rice', 'Souse', 'Sea Fish'],
-                adress: "Sushi Boulevard 1c"
-            },
-            {
-                title: "Philadelphia",
-                price: 70000,
-                weight: "1 kg",
-                ingredients: ['Rice', 'Nori', 'Avokado', 'Salmon'],
-                adress: "Sushi Boulevard 1c"
-            }
-        ]
+      ID: '2',
+      title: 'Philadelphia',
+      price: 6000,
+      category: 'sushi',
+      ingredients: ['rise', 'nori', 'avokado', 'salmon'],
     },
     {
-        category: "Shawarma",
-        items: [
-            {
-                title: "With Chicken",
-                price: 8050,
-                weight: "300 g",
-                category: "Shawarma",
-                ingredients: ['Pita', 'Tomat', 'Cabbage', 'Chicken'],
-                adress: "Shawarma Street 2b"
-            },
-            {
-                title: "With Beef",
-                price: 9050,
-                weight: "300 g",
-                category: "Shawarma",
-                ingredients: ['Pita', 'Tomat', 'Cabbage', 'Beef'],
-                adress: "Shawarma Street 2b"
-            }
-        ]
-    }
-]
+      ID: '3',
+      title: 'Нігірі',
+      price: 6500,
+      category: 'sushi',
+      ingredients: ['rise', 'souce', 'algae', 'tuna'],
+    },
+    {
+      ID: '4',
+      title: 'Хосомакі',
+      price: 5500,
+      category: 'rolls',
+      ingredients: ['rise', 'freshwater fish', 'vegetables', 'algae', 'boiled chicken'],
+    },
+    {
+      ID: '5',
+      title: 'Футомакі',
+      price: 8500,
+      category: 'rolls',
+      ingredients: ['rise', 'cucumbers', 'avocado', 'radish', 'avocado', 'tomatoes'],
+    },
+    {
+      ID: '6',
+      title: 'Спрінг-роли',
+      price: 9000,
+      category: 'rolls',
+      ingredients: ['rise', 'egg omelette', 'souce', 'boiled chicken', 'sea fish'],
+    },
+  ];
 
-{
-    const nameUser = prompt("Enter your name: ") 
-    const emailUser = prompt("Enter your email: ")
-    const phoneUser = prompt("Enter your phone: ")
-    const adressUser = prompt("Enter your adress: ")
-    console.log("==================================================")
-    person = {
-        Name: nameUser,
-        eMail: emailUser,
-        Phone: phoneUser,
-        Adress: adressUser
-    }
-    comandMenu()
-}
+  for (const item of menu) {
+    console.log(`ID: ${item.ID}`);
+    console.log(`Title: ${item.title}`);
+    console.log(`Price: ${item.price}`);
+    console.log(`Category: ${item.category}`);
+    console.log(`Ingredients: ${item.ingredients.join(', ')}`);
+    console.log('\n'); 
+  }
 
-function comandMenu(){
-    console.clear()
-    console.log("Hello,", person.Name)
-    if(statusDelivery !== null){
-        process.stdout.write(colors.green + "STATUS: " + colors.reset);
-        process.stdout.write(statusDelivery + "\n");
-    }
-    console.log("==================================================")
-    console.log("Comand`s MENU")
-    console.log("--------------------------------------------------")
-    console.log(" [MI] - My information")
-    console.log(" [SM] - Show MENU")
-    if(statusDelivery !== null){console.log(" [MO] - My Order")}
-    console.log(" [E] - Exit")
-    console.log("==================================================")
-
-    selectComand()
-}
-
-function menuOfDishes(){
-    console.clear()
-    console.log('\x1b[33m%s\x1b[0m', "What do you WANT?:")
-    console.log("------( MENU )-----------( Price )---")
-    console.log( menu[0].category + ":                           ")
-    console.log(" [S] -", menu[0].items[0].title, "         | ", menu[0].items[0].price/100, "  UAH |  ")
-    console.log(" [H] -", menu[0].items[1].title, "         | ", menu[0].items[1].price/100, "  UAH |  ")
-    console.log( menu[1].category + ":                           ")
-    console.log(" [C] -", menu[1].items[0].title, "     | ", menu[1].items[0].price/100, "  UAH |  ")
-    console.log(" [P] -", menu[1].items[1].title, "   | ", menu[1].items[1].price/100, "  UAH |  ")
-    console.log( menu[2].category + ":                        ")
-    console.log(" [Wc] -", menu[2].items[0].title, "  | ", menu[2].items[0].price/100, " UAH | ")
-    console.log(" [Wb] -", menu[2].items[1].title, "     | ", menu[2].items[1].price/100, " UAH | ")
-    console.log("-------------------------------------")
-    console.log(" [R] - Return to Comand`s MENU")
-    console.log("-------------------------------------")
-    selectOrder()
-}
-
-function returnToMenu() 
-{
-    let comandToExit = prompt("Press Enter to return to the MENU...")
-    comandMenu()
-}
-
-function selectComand(){
-    let choice = prompt(">> ") 
-    switch (choice){
-        case 'MI': console.clear(); console.table(person); returnToMenu(); break 
-        case 'SM': console.clear(); menuOfDishes(); break
-        case 'MO': 
-        console.clear()
-        console.log(myOrder)
-        console.log("Enter 'Y' if received order or 'N' if don`t")
-        let comandToExit2
-        do {
-            comandToExit2 = prompt(">> ")
-            if (comandToExit2 === "Y") {
-                statusDelivery = "Success"
-            } else if (comandToExit2 === "N") {
-                statusDelivery = "In Procces"
-            } else {
-                console.log('\x1b[31m%s\x1b[0m', 'ERROR: Command not found. Try again')
-            }
-        } while (comandToExit2 !== "Y" && comandToExit2 !== "N")
-        comandMenu()
-        break
-        case 'E': console.clear(); process.exit(0); break
-        default: console.log('\x1b[31m%s\x1b[0m', 'ERROR: Command not found. Try again'); selectComand()
-    }
-}
-
-function selectOrder(){
-    let order = prompt(">> ") 
-    switch (order){
-        case 'S': console.clear(); dateAboutOrder(menu[0].category, menu[0].items[0].title, menu[0].items[0].price/100, menu[0].items[0].weight, menu[0].items[0].ingredients, menu[0].items[0].adress); break
-        case 'H': console.clear(); dateAboutOrder(menu[0].category, menu[0].items[1].title, menu[0].items[1].price/100, menu[0].items[1].weight, menu[0].items[1].ingredients, menu[0].items[1].adress); break
-        case 'C': console.clear(); dateAboutOrder(menu[1].category, menu[1].items[0].title, menu[1].items[0].price/100, menu[1].items[0].weight, menu[1].items[0].ingredients, menu[1].items[0].adress); break
-        case 'P': console.clear(); dateAboutOrder(menu[1].category, menu[1].items[1].title, menu[1].items[1].price/100, menu[1].items[1].weight, menu[1].items[1].ingredients, menu[1].items[1].adress); break
-        case 'Wc': console.clear(); dateAboutOrder(menu[2].category, menu[2].items[0].title, menu[2].items[0].price/100, menu[2].items[0].weight, menu[2].items[0].ingredients, menu[2].items[0].adress); break
-        case 'Wb': console.clear(); dateAboutOrder(menu[2].category, menu[2].items[1].title, menu[2].items[1].price/100, menu[2].items[1].weight, menu[2].items[1].ingredients, menu[2].items[1].adress); break
-        case 'R': comandMenu(); break
-        default: console.log('\x1b[31m%s\x1b[0m', 'ERROR: Command not found. Try again'); selectOrder()
-    }
-}
-
-function dateAboutOrder(category, title, price, weight, ingredients, adress){
-    console.clear()
-    console.log("=====================================")
-    console.log("Your order:", category, ":", title)
-    console.log("-------------------------------------")
-    console.log("Weight: ", weight)
-    console.log("Ingredients: ", ingredients)
-    console.log("-------------------------------------")
-    console.log("Adress from: ", adress)
-    console.log("Adress to: ", person.Adress)
-    console.log("-------------------------------------")
-    console.log("Delivery: ", deliveryPrise)
-    console.log("Price: ", price)
-    console.log("Total: ", deliveryPrise + price)
-    console.log("=====================================")
-    console.log("Enter 'Y' for - Confirm the order or 'N' - return to the MENU ")
-
-    let comandToExit3
-    do{
-        comandToExit3 = prompt(">> ")
-        comandToExit3 === "Y" ? confirmOrder() : comandToExit3 === "N" ? menuOfDishes() : console.log('\x1b[31m%s\x1b[0m', 'ERROR: Command not found. Try again')
-    } while (comandToExit3 !== "Y" && comandToExit3 !== "N")
-
-    function confirmOrder(){
-        statusDelivery = "In Procces"
+  function chooseDishes() {
+    let userInputdish;
+    let words;
+    let anotherOrder = '1';
     
-        myOrder = {
-            Recipient: person.Name,
-            yourOrder: title + menu[0].items[0].title,
-            Ingredients: ingredients,
-            adressFrom: adress,
-            adressTo: person.Adress,
-            totalPrice: deliveryPrise + price,
+    do {
+      userInputdish = prompt('Оберіть ID вашого замовлення. Для декількох замовлень введіть ID через пробіл. Щоб обрати одне блюдо декілька разів, просто введіть ID більше одного разу: \ncalifirnia id - 1; \nPhiladelphia id - 2 ;\nНігірі id - 3 ; \nХосомакі id - 4 ; \nФутомакі id - 5 ; \nСпрінг роли id - 6 ; \nВведіть id :' );
+      words = userInputdish.split(' ');
+
+      for (const word of words) {
+        let found = false;
+
+        for (const item of menu) {
+          if (item.ID === word) {
+            console.log(`Ви замовили ${item.title}`);
+            order.dishes.push(item);
+            found = true;
+          }
         }
-        comandMenu()
+
+        if (!found) {
+          console.log(`Товар з ID ${word} не знайдений у меню , спробуйте ще раз :.`);
+        }
+      }
+
+      anotherOrder = prompt('Бажаєте додати ще страви до замовлення? Введіть 1 -- якщо так або 2 -- якщо ні ');
+    } while (anotherOrder === '1');
+  }
+
+  function enterPersonalInfo() {
+    const userInputName = prompt("Введіть ваше прізвище та ім'я через пробіл: ");
+    const userInputemail = prompt("Введіть вашу електронну пошту: ");
+    const userInputadress = prompt("Введіть вашу адресу доставки: ");
+
+    const [lastName, firstName] = userInputName.split(' ');
+    person.name = `${firstName} ${lastName}`;
+    person.email = userInputemail;
+    delivery.to = userInputadress;
+
+    let userInputNumber;
+
+    do {
+      userInputNumber = prompt("Уведіть ваш номер, типу (+380123456789): ");
+    } while (userInputNumber.length !== 13 || userInputNumber[0] !== '+');
+    
+    person.phone = userInputNumber;
+    Object.assign(order, person, delivery);
+  }
+
+  function pricecalculator(){
+    let totalcost = 0;
+
+    for (const dish of order.dishes){
+      totalcost += dish.price;
     }
+
+    order.price = totalcost + delivery.delivery_price;
+  }
+
+  chooseDishes();
+  enterPersonalInfo();
+  pricecalculator();
+  showOrderSummary();
+}
+
+function showOrderSummary(){
+  console.log("\nВаше прізвище та ім'я: " + order.name);
+  console.log("Ваша електронна пошта: " + order.email);
+  console.log("Ваш номер: " + order.phone);
+  console.log("Адреса відправки: " + order.from);
+  console.log("Ваша адреса доставки: " + order.to);
+  
+  console.log("Ваші блюда:");
+
+  for (const dish of order.dishes) {
+    console.log(`Назва: ${dish.title}`);
+  }
+
+  console.log('Ціна замовлення:' + order.price);
+  console.log("Статус замовлення: " + order.status);
+}
+
+function adminMain(){
+
+  function appealToAdministrator(){
+    let chooseaction;
+
+    do {
+      chooseaction = prompt('Якщо бажаєте переглянути замовлення нажміть -- 1, якщо бажаєте змінити статус замовлення -- 2, якщо бажаєте вийти з ролі адміністратора -- 3: ');
+
+      if (chooseaction == '1') {
+        showOrderSummary();
+      } 
+      if (chooseaction == '2') {
+        changeOrderStatus();
+      }
+    } while (chooseaction !== '3' );
+  }
+
+  function changeOrderStatus() {
+    const newStatus = prompt("Обновіть статус замовлення: 1 -- замовлення готується, 2 -- замовлення в дорозі, 3 -- замовлення доставлено, 4 -- схоже щось пішло не туди, \n5 -- нічого не міняти: ");
+    
+    do {
+      if (newStatus == '1') {
+        order.status = 'замовлення готується';
+      }
+      if (newStatus == '2') {
+        order.status = 'замовлення в дорозі';
+      }
+      if (newStatus == '3') {
+        order.status = 'замовлення доставлено';
+      }
+      if (newStatus == '4') {
+        order.status = 'схоже щось пішло не туди';
+      }
+    } while (newStatus !== '5');
+
+    console.log('Теперішній статус замовлення: ' + order.status);
+  }
+
+  appealToAdministrator();
 }
